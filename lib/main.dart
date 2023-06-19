39f15d568a054ed7dea1f28b86ed5ee8cd9310e3
kaxp <kapilsahu232@gmail.com>
2023-06-19 13:02:41 +0700

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:even/config/themes/assets/app_colors.dart';
+import 'package:even/modules/home/home_tab_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Even',
+      home: HomeTabBar(),
+    );
+  }
+}
+
