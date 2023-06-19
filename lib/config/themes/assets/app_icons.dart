171c19c70b6ef2c1c68bd3afc1ac21a6d0c5cdce
kaxp <kapilsahu232@gmail.com>
2023-06-19 13:03:59 +0700

    Add app icons

---
+++ lib/config/themes/assets/app_icons.dart
@@ -0,0 +1,13 @@
+mixin AppIcons {
+  static const icTabHomeOff = 'assets/icons/ic_tab_home_OFF.svg';
+  static const icTabHistoryOff = 'assets/icons/ic_tab_history_OFF.svg';
+  static const icTabMindfulnessOff = 'assets/icons/ic_tab_mindfulness_OFF.svg';
+  static const icTabAccountOff = 'assets/icons/ic_tab_account_OFF.svg';
+  static const icLine = 'assets/icons/ic_line.svg';
+  static const icPlus = 'assets/icons/ic_plus.svg';
+  static const icTabShapeOne = 'assets/icons/ic_tab_shape_one.svg';
+  static const icTabShapeTwo = 'assets/icons/ic_tab_shape_two.svg';
+  static const icClose = 'assets/icons/ic_close.svg';
+  static const icTabTwo = 'assets/icons/tab_two.svg';
+  static const icTabOne = 'assets/icons/tab_one.svg';
+}
