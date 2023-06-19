6b14a6c2826a10c60b506ecec04c66e8ec050180
kaxp <kapilsahu232@gmail.com>
2023-06-19 13:04:12 +0700

    Add app images

---
+++ lib/config/themes/assets/app_images.dart
@@ -0,0 +1,5 @@
+mixin AppImages {
+  static const icHomeHospital = 'assets/images/home_hospital.png';
+  static const icConsultationImage = 'assets/images/consultation_image.png';
+  static const icPrescription = 'assets/images/prescription.png';
+}
